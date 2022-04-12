ea75e5f629b5d6476893fbc52732ecabb94b2e81
e-normous Mario <e-normous@outlook.com>
2022-04-12 21:49:41 +0200

    Slightly changed the title of the program

--- A24_FactorFinder.py
+++ A24_FactorFinder.py
@@ -1,4 +1,4 @@
-#project 16: Factor Finder
+#project 24: Factor Finder
 """A number’s factors are any two other numbers that, when multiplied with each other,
 produce the number. For example, 2 × 13 = 26, so 2 and 13 are factors of 26. Also, 1 × 26 = 26,
 so 1 and 26 are also factors of 26. Therefore, we say that 26 has four factors: 1, 2, 13, and 26.
